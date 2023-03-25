938232d573223bb11aa4806966ed79e1b3d41f97
simonsteiger <simon.j.steiger@gmail.com>
2023-03-25 20:58:11 +0100

    New theme
    based on favicon

--- app/logic/theme.R
+++ app/logic/theme.R
@@ -1,7 +1,7 @@
 
 box::use(
   thematic[thematic_shiny, thematic_on, font_spec],
-  bslib[bs_theme],
+  bslib[bs_theme, font_google],
 )
 
 thematic_shiny()
@@ -12,6 +12,20 @@ thematic_on(
   font = font_spec("Fira Sans", scale = 1.75)
   )
 
-theme <- bs_theme(
-  bootswatch = "minty"
-  )
+theme <-  bs_theme(
+  version = 5,
+  bg = "#ffffff",
+  fg = "#565b85",
+  primary = "#E0A969",
+  secondary = "#4161ab",
+  success = "#38FF12",
+  info = "#e8e9f3",
+  warning = "#c26130",
+  danger = "#FF00E3",
+  base_font = font_google("Fira Sans"),
+  heading_font = font_google("Fira Sans"),
+  code_font = font_google("Fira Sans")
+)
+
+#E0A969
+#9B9EB9
